cead7ae9d49cf45606cc9b8256bb3f79932818ee
Nishant Desai <nishantdesai1306@gmail.com>
2019-06-29 16:50:17 +0530

    init setup

---
+++ lib/default_app_bar.dart
@@ -0,0 +1,109 @@
+import 'package:app_bar_animation/my_painter.dart';
+import 'package:app_bar_animation/search_bar.dart';
+import 'package:flutter/material.dart';
+
+class DefaultAppBar extends StatefulWidget implements PreferredSizeWidget {
+  @override
+  Size get preferredSize => Size.fromHeight(56.0);
+
+  @override
+  _DefaultAppBarState createState() => _DefaultAppBarState();
+}
+
+class _DefaultAppBarState extends State<DefaultAppBar> with SingleTickerProviderStateMixin {
+  double rippleStartX, rippleStartY;
+  AnimationController _controller;
+  Animation _animation;
+  bool isInSearchMode = false;
+
+  @override
+  initState() {
+    super.initState();
+
+    _controller = AnimationController(vsync: this, duration: Duration(milliseconds: 800));
+    _animation = Tween(begin: 0.0, end: 1.0).animate(_controller);
+    _controller.addStatusListener(animationStatusListener);
+  }
+
+  animationStatusListener(AnimationStatus animationStatus) {
+    if (animationStatus == AnimationStatus.completed) {
+      setState(() {
+       isInSearchMode = true; 
+      });
+    }
+  }
+
+  void onSearchTapUp(TapUpDetails details) {
+    setState(() {
+      rippleStartX = details.globalPosition.dx;
+      rippleStartY = details.globalPosition.dy;
+    });
+
+    print("pointer location $rippleStartX, $rippleStartY");
+    _controller.forward();
+  }
+
+  cancelSearch() {
+    setState(() {
+      isInSearchMode = false;
+    });
+
+    onSearchQueryChange('');
+    _controller.reverse();
+  }
+
+  onSearchQueryChange(String query) {
+    print('search $query');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+
+    return Stack(
+      children: [
+        AppBar(
+          title: Text("Flutter App"),
+          actions: <Widget>[
+            GestureDetector(
+              child: IconButton(
+                icon: Icon(
+                  Icons.search,
+                  color: Colors.white,
+                ),
+              ),
+              onTapUp: onSearchTapUp,
+            ),
+            IconButton(
+              icon: Icon(Icons.more_vert),
+              onPressed: () {},
+            )
+          ],
+        ),
+
+        AnimatedBuilder(
+          animation: _animation,
+          builder: (context, child) {
+            return CustomPaint(
+              painter: MyPainter(
+                containerHeight: widget.preferredSize.height,
+                center: Offset(rippleStartX ?? 0, rippleStartY ?? 0),
+                radius: _animation.value * screenWidth,
+                context: context,
+              ),
+            );
+          },
+        ),
+
+        isInSearchMode ? (
+          SearchBar(
+            onCancelSearch: cancelSearch,
+            onSearchQueryChanged: onSearchQueryChange,
+          )
+        ) : (
+          Container()
+        )
+      ]
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:app_bar_animation/default_app_bar.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'App'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: DefaultAppBar(), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
